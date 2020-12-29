71adbd690a425d2cc13aa29bac7874598c077564
imsoumya18 <soumyadeep184@gmail.com>
2020-12-29 17:20:32 +0530

    fix

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=F:\Coding\mail_automation\venv
+set VIRTUAL_ENV=F:\Github\mail_automation\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
